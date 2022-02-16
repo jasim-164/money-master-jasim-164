bb15d687d5f3a747cbb049532e1ad1125c1a6149
jasim-164 <jasimuddincse1703164@gmail.com>
2022-02-17 03:23:35 +0600

    money calculate done

--- JavaSrcipt/script.js
+++ JavaSrcipt/script.js
@@ -4,38 +4,48 @@ const inputIncome=document.getElementById("inputIncome");
 const inputFood=document.getElementById("inputFood");
 const inputRent=document.getElementById("inputRent");
 const inputClothes=document.getElementById("inputClothes");
-const Expenses=document.getElementById("total_expenses");
 //for calculte balance after expenses
+const Expenses=document.getElementById("total_expenses");
 const balance=document.getElementById("balance");
-const inputSave=document.getElementById("inputSave");
 // for saving amount
+const inputSave=document.getElementById("inputSave");
 const savingAmount=document.getElementById("savingAmount");
 const RemainingAmount=document.getElementById("RemainingAmount");
 
 
+//variable declare
+var totalExpenses;
+var remainingBalance;
+var totalIncome;
+var saveBalance
+//balance variable
 
-let totalExpenses;
-let remainingBalance;
-let totalIncome;
 
 function calculatePercentage(totalIncome,saveBalance){
 var percentage =(totalIncome*saveBalance) / (100);
 return percentage;
 }
+
 document.getElementById("calculate").addEventListener("click",function(e) {
+   
     totalExpenses= parseInt(inputRent.value)+parseInt(inputClothes.value)+parseInt(inputFood.value);
     Expenses.innerText=totalExpenses;
 
     totalIncome=parseInt(inputIncome.value);
     remainingBalance=totalIncome-totalExpenses;
     balance.innerText=remainingBalance;
-
-    var saveBalance=inputSave.innerText;
-
-    document.getElementById("save-button").addEventListener("click",function(e) {
-        var value=calculatePercentage(totalIncome,saveBalance);
-
-
-    })
 })
+
+document.getElementById("saveButton").addEventListener("click",function(e) {
+    saveBalance=parseInt(inputSave.value);
+    console.log("totalIncome",totalIncome);
+    console.log("savebalance2", saveBalance);
+    let val= (totalIncome*saveBalance) / (100);
+    //var amount=calculatePercentage(totalIncome,saveBalance);
+    console.log(val);
+    savingAmount.innerText=val;
+    RemainingAmount.innerText =remainingBalance-val;
+
+})
+
 
