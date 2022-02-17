b4b4750026b21cfca7fdc0ed4e9a1b8e77e2f6f0
jasim-164 <jasimuddincse1703164@gmail.com>
2022-02-17 13:00:14 +0600

    js work done

--- JavaSrcipt/script.js
+++ JavaSrcipt/script.js
@@ -11,50 +11,108 @@ const balance=document.getElementById("balance");
 const inputSave=document.getElementById("inputSave");
 const savingAmount=document.getElementById("savingAmount");
 const RemainingAmount=document.getElementById("RemainingAmount");
-
-
+//error message load
+var x = document.getElementById('error-message');  
+var y = document.getElementById('error-message2'); 
 //variable declare
 var totalExpenses;
 var remainingBalance;
 var totalIncome;
-var saveBalance
+var saveBalance;
+var amount;
 //balance variable
-
+var allErrors=[];
 
 function calculatePercentage(totalIncome,saveBalance){
 var percentage =(totalIncome*saveBalance) / (100);
 return percentage;
 }
 
+function checkInput()
+{
+    //number check is this is number
+    if(isNaN(inputRent.value))
+    {
+        allErrors.push("input is not number");
+    }
+    if(isNaN(inputClothes.value))
+    {
+        allErrors.push("input is not number");
+    }
+    if(isNaN(inputFood.value))
+    {
+        allErrors.push("input is not number");
+    }
+    //negative number check 
+    if(parseInt(inputRent.value)<0 || parseInt(inputClothes.value)<0 ||parseInt(inputFood.value)<0 )
+    {
+        allErrors.push("input number is negative");
+    }
+
+    //for saving input valiadtion check
+    if(remainingBalance<amount)
+    {
+        allErrors.push("negative current balance ");
+    }
+    if(isNaN(inputSave.value))
+    {
+        allErrors.push("input is not number");
+    }
+    if(inputSave.value<0)
+    {
+        allErrors.push("input is not number");
+    }
+
+}
+
 document.getElementById("calculate").addEventListener("click",function(e) {
+    checkInput(); 
+    if(allErrors.length>0)
+    {
+        Expenses.innerText="Invalid";
+        balance.innerText="Invalid";
+           
+        x.style.display = 'inline-block';//error message display
+        allErrors=[];
+       
+    }
    
+    else{
+    x.style.display = 'none';//error hidden
     totalExpenses= parseInt(inputRent.value)+parseInt(inputClothes.value)+parseInt(inputFood.value);
     Expenses.innerText=totalExpenses;
 
     totalIncome=parseInt(inputIncome.value);
     remainingBalance=totalIncome-totalExpenses;
     balance.innerText=remainingBalance;
+    }
 })
-
+allErrors=[];
 document.getElementById("saveButton").addEventListener("click",function(e) {
     saveBalance=parseInt(inputSave.value);
-    //console.log("totalIncome",totalIncome);
-    //console.log("savebalance2", saveBalance);
-    //let val= (totalIncome*saveBalance) / (100);
-    var errors= [];
-    var amount=calculatePercentage(totalIncome,saveBalance);
-    console.log(amount);
-    savingAmount.innerText=amount;
-    RemainingAmount.innerText =remainingBalance-amount;
 
-    if(RemainingAmount.innerText<0)
+    amount=calculatePercentage(totalIncome,saveBalance);
+
+    checkInput(); //error function called
+
+    // if(RemainingAmount.innerText<0)
+    // {
+    //     errors.push("Please give correct input thats why saving amount is not greater than current amount")
+    // }
+
+    if(allErrors.length>0)
     {
-        errors.push("Please give positive integer");
-        alert("Please give positive integer");
+        y.style.display = 'inline-block';
+        savingAmount.innerText="Invalid input";
+        RemainingAmount.innerText="Invalid input";
+        allErrors=[];
     }
-    if(errors.length>0)
+    else
     {
-        console.log("errors");
+        y.style.display = 'none';
+        //console.log(amount);
+        savingAmount.innerText=amount;
+        RemainingAmount.innerText =remainingBalance-amount;
     }
 
 })
